66b106a1951041059cf8c9e70b87a3f6bf9ffd14
Rodrigo Iván Miranda Reidenbach <rodrigomreidenbach@gmail.com>
2023-07-29 20:49:05 -0400

    lfg

--- src/components/carrusel.vue
+++ src/components/carrusel.vue
@@ -2,17 +2,21 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="3000">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="../assets/logo.png" alt="Servicio 1">
+      <img src="../assets/tatuajes.png" alt="Servicio 1">
       <div class="carousel-caption">
-        <h3>Servicio 1</h3>
-        <p>Descripción del servicio 1.</p>
+
       </div>
     </div>
     <div class="carousel-item">
-      <img src="../assets/logo.png" alt="Servicio 2">
+      <img src="../assets/perforaciones.png" alt="Servicio 2">
       <div class="carousel-caption">
-        <h3>Servicio 2</h3>
-        <p>Descripción del servicio 2.</p>
+   
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/borradoLaser.png" alt="Servicio 2">
+      <div class="carousel-caption">
+   
       </div>
     </div>
     <!-- Agrega más elementos de carousel-item según sea necesario -->
